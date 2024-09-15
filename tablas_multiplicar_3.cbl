780718af9e632c686bf0fc786a03f24177ab7765
Adrian Gonzalez <adriangonzalezmartos@proton.me>
2024-09-15 21:50:57 +0200

    Tablas de multiplicar 3

---
+++ tablas_multiplicar_3.cbl
@@ -0,0 +1,30 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. TABLAS-MULTIPLICAR.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       01  SALIR PIC X.
+       01  NUMERO PIC 99.
+       01  MULT PIC 99.
+       01  RES PIC 999.
+
+       PROCEDURE DIVISION.
+       INICIO.
+           DISPLAY "Para multiplicar pulsa ENTER".
+           DISPLAY "Para salir introduce 'S'".
+           ACCEPT SALIR.
+           IF SALIR = "S"
+               STOP RUN
+           END-IF.
+           DISPLAY "Introduce un numero".
+           ACCEPT NUMERO.
+           DISPLAY "Tabla del " NUMERO.
+           MOVE 0 TO MULT
+           PERFORM CALCULAR UNTIL MULT > 10.
+           GO TO INICIO.
+
+       CALCULAR.
+               COMPUTE RES = NUMERO * MULT
+               DISPLAY NUMERO " * " MULT " = " RES
+               ADD 1 TO MULT.
+       END PROGRAM TABLAS-MULTIPLICAR.
